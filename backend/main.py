a16e334d1826e25f09eecd2bd2da7d231094cb5a
Manuel Martínez <manuel.mtzv816@gmail.com>
2023-06-18 17:26:24 -0600

    feat: add main routes and file structure

--- backend/main.py
+++ backend/main.py
@@ -13,3 +13,28 @@ app.add_middleware(
 @app.get("/")
 def root():
     return {"message": "Root route"}
+
+
+@app.get("/api/todo")
+async def get_todo():
+    return 1
+
+
+@app.get("/api/todo/{id}")
+async def get_todo_by_id(id):
+    return id
+
+
+@app.post("/api/todo/")
+async def add_todo(todo):
+    return 1
+
+
+@app.put("/api/todo/{id}")
+async def update_todo(id, data):
+    return id
+
+
+@app.delete("/api/todo/{id}")
+async def delete_todo_by_id(id):
+    return 1
